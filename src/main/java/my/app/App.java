7f0d251937a8045b914609eb44493928fada3ae3
Allen Day <allenday@allenday.com>
2022-08-02 16:39:47 +0800

    deps

--- src/main/java/my/app/App.java
+++ src/main/java/my/app/App.java
@@ -1,13 +1,17 @@
 package my.app;
+import org.commonmark.node.*;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse("This is *Sparta*");
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
     }
 }
