85ad9b9cc1b5bdb9d790f2a2f159a573cf5b8aa3
Allen Day <allenday@allenday.com>
2022-08-16 19:14:41 +0800

    complete frontmatter coverage

--- src/test/java/my/app/AppTest.java
+++ src/test/java/my/app/AppTest.java
@@ -38,6 +38,7 @@ public class AppTest
         String directory = "site/content/english/blog";
         List<File> files = listFiles(directory);
 
+        Map<String,Map<String,Boolean>> res = new HashMap<String,Map<String,Boolean>>();
         for (File f : files) {
             String txt = Files.readString(Path.of(f.toString()));
 
@@ -48,39 +49,120 @@ public class AppTest
 
             //TODO enhanceFrontMatterWithGoogleAnalytics()
             //TODO enhanceFrontMatterWithAhrefs()
-            validateFrontMatter(frontMatter, f.toString());
-            
+
+            Map<String,Boolean> r = validateFrontMatter(frontMatter);
+            res.put(f.toString(), r);
 
             //HtmlRenderer renderer = HtmlRenderer.builder().build();
             //String res = renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
             //System.err.println(res);
         }
+
+        for (String k : res.keySet()) {
+          System.err.println(k);
+          Map<String,Boolean> h = res.get(k);
+          for (String r : h.keySet()) {
+            System.err.println("\t"+r+"\t"+h.get(r));
+          }
+        }
     }
 
-    private static void validateFrontMatter(final Map<String, List<String>> frontMatter, String filename) {
-        assert frontMatter.containsKey("type") : filename + " has no type";
-        List<String> types = frontMatter.get("type");
-        assert types.size() == 1 : filename + " expected a single type";
-        String type = types.get(0);
-        assert type.compareTo("post") == 0 : filename + " expected type 'post', but got '" + type + "'";
+    private static Map<String,Boolean> validateFrontMatter(final Map<String, List<String>> frontMatter) throws IOException {
+        Set<String> catWhitelist = new HashSet<String>();
+        Set<String> tagWhitelist = new HashSet<String>();
+        String[] cat_ = Files.readString(Path.of("site/content/english/meta/category.txt")).split("\n");
+        String[] tag_ = Files.readString(Path.of("site/content/english/meta/tag.txt")).split("\n");
+        for (String x : cat_) { catWhitelist.add(x); }
+        for (String x : tag_) { tagWhitelist.add(x); }
 
-        assert frontMatter.containsKey("author") : filename + " has no author";
-        List<String> authors = frontMatter.get("author");
-        assert authors.size() == 1 : filename + " expected a single author";
-        String author = authors.get(0);
-        assert author.length() > 0 : filename + " expected a non-zero character author";
+        Map<String,Boolean> res = new HashMap<String,Boolean>();
 
-        assert frontMatter.containsKey("title") : filename + " has no title";
-        List<String> titles = frontMatter.get("title");
-        assert titles.size() == 1 : filename + " expected a single title";
-        String title = titles.get(0);
-        assert title.length() >= 50 && title.length() <= 70 : filename + " expected a 50-70 character title";
 
-        assert frontMatter.containsKey("meta_title") : filename + " has no meta_title";
-        List<String> metatitles = frontMatter.get("meta_title");
-        assert metatitles.size() == 1 : filename + " expected a single meta_title";
-        String metatitle = metatitles.get(0);
-        assert metatitle.length() >= 150 && metatitle.length() <= 160 : filename + " expected a 150-160 character meta_title";
+        res.put("frontmatter.type", false);
+        if (frontMatter.containsKey("type") && frontMatter.get("type").size() == 1) {
+          String type = frontMatter.get("type").get(0);
+          res.put("frontmatter.type", type.compareTo("post") == 0);
+        }
+
+        res.put("frontmatter.author", false);
+        if (frontMatter.containsKey("author") && frontMatter.get("author").size() == 1) {
+          String author = frontMatter.get("author").get(0);
+          res.put("frontmatter.author", author.length() > 0);
+        }
+
+        res.put("frontmatter.title", false);
+        if (frontMatter.containsKey("title") && frontMatter.get("title").size() == 1) {
+          String title = frontMatter.get("title").get(0);
+          res.put("frontmatter.title", title.length() >= 50 && title.length() <= 60);
+        }
+
+        res.put("frontmatter.meta_title", false);
+        if (frontMatter.containsKey("meta_title") && frontMatter.get("meta_title").size() == 1) {
+          String meta = frontMatter.get("meta_title").get(0);
+          res.put("frontmatter.meta_title", meta.length() >= 50 && meta.length() <= 60);
+        }
+
+        res.put("frontmatter.description", false);
+        if (frontMatter.containsKey("description") && frontMatter.get("description").size() == 1) {
+          String meta = frontMatter.get("description").get(0);
+          res.put("frontmatter.description", meta.length() >= 150 && meta.length() <= 160);
+        }
+
+        res.put("frontmatter.keywords", false);
+        if (frontMatter.containsKey("keywords") && frontMatter.get("keywords").size() == 1) {
+          String meta = frontMatter.get("keywords").get(0);
+          res.put("frontmatter.keywords", meta.split(",").length >= 3 && meta.split(",").length <= 10);
+        }
+
+        res.put("frontmatter.image", false);
+        if (frontMatter.containsKey("image") && frontMatter.get("image").size() == 1) {
+          String meta = frontMatter.get("image").get(0);
+          res.put("frontmatter.image", meta.length() > 0);
+        }
+
+        res.put("frontmatter.date", false);
+        if (frontMatter.containsKey("date") && frontMatter.get("date").size() == 1) {
+          String date = frontMatter.get("date").get(0);
+          res.put("frontmatter.date", date.length() > 0);
+        }
+
+        res.put("frontmatter.bg_image", false);
+        if (frontMatter.containsKey("bg_image") && frontMatter.get("bg_image").size() == 1) {
+          String img = frontMatter.get("bg_image").get(0);
+          res.put("frontmatter.bg_image", img.length() > 0);
+        }
+
+        res.put("frontmatter.blog_category", false);
+        if (frontMatter.containsKey("blog_category") && frontMatter.get("blog_category").size() == 1) {
+          String cat = frontMatter.get("blog_category").get(0);
+          res.put("frontmatter.blog_category", cat.length() > 0);
+        }
+
+        res.put("frontmatter.tags", false);
+        if (frontMatter.containsKey("tags")) {
+          List<String> tags = frontMatter.get("tags");
+          Boolean stat = true;
+          for (String tag : tags) {
+            if (! tagWhitelist.contains(tag)) {
+              stat = false;
+            }
+          }
+          res.put("frontmatter.tags", stat);
+        }
+
+        res.put("frontmatter.category", false);
+        if (frontMatter.containsKey("category")) {
+          List<String> cats = frontMatter.get("category");
+          Boolean stat = true;
+          for (String cat : cats) {
+            if (! catWhitelist.contains(cat)) {
+              stat = false;
+            }
+          }
+          res.put("frontmatter.category", stat);
+        }
+
+        return res;
     }
 
 
